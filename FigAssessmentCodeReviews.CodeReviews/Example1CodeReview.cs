7fccc0229d3550382e8451abedeccfe5edab4bdc
Mason Watson <masonxwork@gmail.com>
2026-02-08 13:35:32 -0600

    adding server-side code reviews

---
+++ FigAssessmentCodeReviews.CodeReviews/Example1CodeReview.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace FigAssessmentCodeReviews.CodeReviews
+{
+    /*
+     * These are the things that I'm going to assume:
+     * .NET version 8.0
+     * We have DI configuration in our root file
+     * We are not using global exception handling
+    */
+
+    // Would you mind making an Interface that this service could inherit? This promotes decoupling for the components that want an
+    // instance of this service. It could also make testing the components that utilize this service easier, as we could make a 'TestService'
+    // that inherits said interface, then tell the DI config to use that 'TestService'.
+    public class UserService
+    {
+        // I believe there should be an appsettings value that we can pull in, instead of hardcoding this! If there isn't, would
+        // you mind making one?
+        private readonly string _connectionString = "Server=prod-db01;Database=UserDB;User Id=sa;Password=MySecretPassword123!;";
+
+        // Small change, but since there is a possibilty of returning a null value, could we make the return type nullable?
+        // Can we also pass in CancellationTokens from our controllers and feed them downstream to our SqlClient calls?
+        public async Task<User> GetUserByIdAsync(int userId)
+        {
+            // Great thinking, but I would let exceptions bubble up, and have the controller handle them and logging!
+            try
+            {
+                // Could we add an await at the beginning of this line, before 'using var'? The connection would block on cleanup,
+                // so adding await will free the thread to do other stuff in the mean time!
+                using var connection = new SqlConnection(_connectionString);
+
+                // Could we add an await at the begining of this line and change .Open() to .OpenAsync()? Currently, this would
+                // block while talking to SQL server, which could take some time!
+                connection.Open();
+
+                // I really love string interpolation, it looks so clean, but in this situation it could unfortunately
+                // leave us vulnerable to SQL Injection. Would you mind using a Parameterized Query?
+                // Also, SELECT * is perfect if we know we need every column, but it could affect performance if we don't
+                // need all of them! Would you mind declaring the columns that we need in the select list?
+                var query = $"SELECT * FROM Users WHERE Id = {userId}";
+                using var command = new SqlCommand(query, connection);
+
+                // Sheesh! .ExecuteReader() seems a little harsh... I don't know what the reader did, but could we add an await
+                // after the equals sign and use .ExecuteReaderAsync() so I don't have to be around for that?
+                // Could we also add an await at the begining of this line, before 'using var'? Currently, the reader would
+                // block on cleanup, so adding await will let the thread do other stuff in the mean time!
+                // It's also good practice to include the CommandBehavior when calling ExecuteReader, as it can improve performance!
+                using var reader = command.ExecuteReader();
+
+                // Could we add an await before 'reader' and change .Read() to .ReadAsync()?
+                // Currently, this would wait on the network for the next row, so we want to release the thread if it does have to wait
+                if (reader.Read())
+                {
+                    return new User
+                    {
+                        // Indexing and casting totally works, but it could be a little bit dangerous. Do you think we could use ordinals
+                        // and typed getters to avoid DBNull issues and unsafe casts?
+                        Id = (int)reader["Id"],
+                        Username = reader["Username"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        Password = reader["Password"].ToString(), // Returning password hash
+                        CreatedDate = (DateTime)reader["CreatedDate"],
+                        IsActive = (bool)reader["IsActive"],
+                        Role = reader["Role"].ToString()
+                    };
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                // Log exception somewhere
+
+                throw ex;
+            }
+        }
+
+        public async Task<bool> ValidateUserAsync(string username, string password)
+        {
+            // Do you mind adding some validation for username and password, just to verify that we have something
+            // to search on?
+
+            // Looks like this needs the same update mentioned in lines 28-29!
+            using var connection = new SqlConnection(_connectionString);
+
+            // This line needs the same update mentioned in lines 32-33!
+            connection.Open();
+
+            // This one needs the same update as mentioned in lines 36-37!
+            var query = $"SELECT COUNT(*) FROM Users WHERE Username = '{username}' AND Password = '{password}'";
+            using var command = new SqlCommand(query, connection);
+
+            // Oh no, they're getting Scalar too! Could we update this line using the mentioned edit in lines 43-47,
+            // except we'll use .ExecuteScalarAsync()
+            var result = (int)command.ExecuteScalar();
+            return result > 0;
+        }
+
+        // This line would need the update mentioned in line 21-22
+        public async Task<User> CreateUserAsync(string username, string email, string password)
+        {
+            // Do you mind adding some validation for username, email, and password since all of these fields are required?
+
+            // Looks like this needs the same update mentioned in lines 28-29!
+            using var connection = new SqlConnection(_connectionString);
+
+            // This line needs the same update mentioned in lines 32-33!
+            connection.Open();
+
+            // This one needs the same update as mentioned in lines 36-37!
+            // Also, both queries in this block look fantastic, but this could potentially be a high-traffic endpoint. Would you mind appending
+            // the second query to the end of the first, so we only have to make one call over the network? 
+            var insertQuery = $"INSERT INTO Users (Username, Email, Password, CreatedDate, IsActive) VALUES ('{username}', '{email}', '{password}', GETDATE(), 1)";
+            using var command = new SqlCommand(insertQuery, connection);
+
+            // Because of the change mentioned in lines 111-112 and since we don't want to block while the query executes,
+            // would you mind changing this call to be 'using var reader = await command.ExecuteReaderAsync();'?
+            command.ExecuteNonQuery();
+
+            // Because of the changes mentioned in lines 111-112 and 116-117, we should be able to remove lines 122-124
+            // Get the newly created user
+            var selectQuery = $"SELECT TOP 1 * FROM Users WHERE Username = '{username}' ORDER BY CreatedDate DESC";
+            using var selectCommand = new SqlCommand(selectQuery, connection);
+            using var reader = selectCommand.ExecuteReader();
+
+            // This looks like it needs the same update mentioned in line 50-51!
+            if (reader.Read())
+            {
+                return new User
+                {
+                    // These lines would need the same update mentioned in lines 56-57!
+                    Id = (int)reader["Id"],
+                    Username = reader["Username"].ToString(),
+                    Email = reader["Email"].ToString(),
+                    Password = reader["Password"].ToString(),
+                    CreatedDate = (DateTime)reader["CreatedDate"],
+                    IsActive = (bool)reader["IsActive"]
+                };
+            }
+
+            return null;
+        }
+    }
+
+    // I might be missing something, but if we're only using this class for returning data from the database
+    // do you mind changing all instances of set; to init;? Since our database is our source of truth, we want these
+    // properties to be immutable! We should also add the required modifier to the properties that are required
+    // and if a string property is not required, we should default it with an empty string! 
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ FigAssessmentCodeReviews.CodeReviews/Example2CodeReview .cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FigAssessmentCodeReviews.CodeReviews
+{
+    /*
+     * These are the things that I'm going to assume:
+     * .NET version 8.0
+     * We have DI configuration in our root file
+     * We are not using global exception handling
+    */
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // Can the query parameters be replaced with a query object? Query parameters will be more extensible/readable this way!
+        // I love the flexibility of IActionResult, but since our return is always the same do you mind making a class for the return type? ActionResult<T> promotes type safety!
+        // Would you also mind passing in a CancellationToken from here all the way to our SqlClient code? This will allow us to stop if the client cancels the request!
+        [HttpGet]
+        public async Task<IActionResult> GetProducts(string category = null, int page = 1, int pageSize = 10, decimal? minPrice = null, decimal? maxPrice = null)
+        {
+            // Would you mind wrapping this block in a try/catch? That way we can properly tell any clients and log if something went wrong.
+            // If this is how things already exist, let's keep lines 32-59 the same, but if GetAllProductsAsync() was something that you made, would you mind making a generalized service
+            // method called GetProducts() that we can pass our QueryObject to? This will help us reduce the complexity of filtering logic, because we can tell the query to filter!
+            var allProducts = await _productService.GetAllProductsAsync();
+
+            // Logic looks awesome, but filtering with SQL should be more performant!
+            // Filter products
+            var filteredProducts = new List<Product>();
+            foreach (var product in allProducts)
+            {
+                bool includeProduct = true;
+
+                if (!string.IsNullOrEmpty(category) && product.Category != category)
+                    includeProduct = false;
+
+                if (minPrice.HasValue && product.Price < minPrice.Value)
+                    includeProduct = false;
+
+                if (maxPrice.HasValue && product.Price > maxPrice.Value)
+                    includeProduct = false;
+
+                if (includeProduct)
+                    filteredProducts.Add(product);
+            }
+
+            // Logic looks great, but we can use OFFSET/FETCH in our SQL! It should be more performant!
+            // Apply pagination
+            var pagedProducts = filteredProducts
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            // Returning a defined object here might be the best thing to do, since the return type is staying the same!
+            return Ok(new
+            {
+                Products = pagedProducts,
+                TotalCount = filteredProducts.Count,
+                Page = page,
+                PageSize = pageSize
+            });
+        }
+
+        // This looks like it might need the same update as lines 24-25!
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] ProductCreateRequest request)
+        {
+            // Love the 400 error handling!
+            // Manual validation instead of using data annotations
+            if (string.IsNullOrEmpty(request.Name))
+            {
+                return BadRequest("Product name is required");
+            }
+
+            if (request.Name.Length < 3)
+            {
+                return BadRequest("Product name must be at least 3 characters");
+            }
+
+            if (request.Price <= 0)
+            {
+                return BadRequest("Product price must be greater than 0");
+            }
+
+            if (string.IsNullOrEmpty(request.Category))
+            {
+                return BadRequest("Product category is required");
+            }
+
+            var product = new Product
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Price = request.Price,
+                Category = request.Category,
+                InStock = request.InStock
+            };
+
+            // Would you mind wrapping this service and the return in a try/catch? That way we can tell any clients and log if something went wrong.
+            var createdProduct = _productService.CreateProductAsync(product);
+
+            return Ok(createdProduct);
+        }
+
+        // This is great, but since it's operating as filter for _productService.GetAllProductsAsync(), I believe we can use the
+        // new generalized GetProducts() mentioned in line 38 with the category as a query param
+        [HttpGet("category/{categoryName}")]
+        public async Task<IActionResult> GetProductsByCategory(string categoryName)
+        {
+            var allProducts = await _productService.GetAllProductsAsync();
+
+            var categoryProducts = new List<Product>();
+            for (int i = 0; i < allProducts.Count; i++)
+            {
+                if (allProducts[i].Category.ToLower() == categoryName.ToLower())
+                {
+                    categoryProducts.Add(allProducts[i]);
+                }
+            }
+
+            return Ok(categoryProducts);
+        }
+
+        // This is prettys similar to the comment mentioned on lines 112-113! We should be able to make the serach term a query param
+        // and use a SQL "LIKE '%Name%' OR '%Description%'". This should be more performant!
+        [HttpGet("search")]
+        public IActionResult SearchProducts(string searchTerm)
+        {
+            var allProducts = _productService.GetAllProductsAsync();
+
+            var matchingProducts = new List<Product>();
+            foreach (var product in allProducts.Result)
+            {
+                if (product.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                    product.Description.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                {
+                    matchingProducts.Add(product);
+                }
+            }
+
+            return Ok(matchingProducts);
+        }
+    }
+
+    // Would you mind making the strings nullable?
+    public class ProductCreateRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public bool InStock { get; set; }
+    }
+
+    // I might be missing something, but if we're only using this class for returning data from the database
+    // do you mind changing all instances of set; to init;? Since our database is our source of truth, we want these
+    // properties to be immutable! We should also add the required modifier to the properties that are required
+    // and if a string property is not required, we should default it with an empty string! 
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public bool InStock { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllProductsAsync();
+        Task<Product> CreateProductAsync(Product product);
+    }
+}
